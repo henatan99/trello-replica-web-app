e0773290d63b3b208a02d2e93fd7be65e49219af
Jim Kratus <jimkratus9@gmail.com>
2022-07-12 07:45:39 -0400

    Add and styled the SideNav element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import SideNav from './Components/SideNav';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SideNav />
     </div>
   );
 }
